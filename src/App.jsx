c1a8b34e99f3632fcba8655e10917e303d9162f7
Fatima-weoveri <fatima@we-over-i.com>
2025-12-01 14:36:56 +0500

    Updated deployed url

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
-const API_BASE =
-  "https://storia-ai-docker-image-697653045832.us-central1.run.app"; // change to your deployed URL later
+const API_BASE = "https://storia-ai-docker-image-bkavgfk3ha-od.a.run.app";
 
 function App() {
   const [question, setQuestion] = useState("");
